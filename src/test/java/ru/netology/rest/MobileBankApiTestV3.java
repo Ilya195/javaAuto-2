4730eb0eb39e0b6c723f2174aede48f2e1e09862
Ilya195 <vodoley_agent@inbox.ru>
2022-08-06 16:02:27 +0600

    Created new test USD

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -24,7 +24,25 @@ class MobileBankApiTestV3 {
           .contentType(ContentType.JSON)
           .body("", hasSize(3))
           .body("[0].currency", equalTo("RUB"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
-      ;
+          .body("[0].balance", greaterThanOrEqualTo(0));
+    }
+
+    @Test
+    void shouldReturnDemoAccountsUSD() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[1].currency", equalTo("USD"))
+                .body("[0].balance", greaterThanOrEqualTo(0));
     }
 }
